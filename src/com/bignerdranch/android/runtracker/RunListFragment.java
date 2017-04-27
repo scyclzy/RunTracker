d1ff322f12ce75276b2f0cd6225a6f74124d49d4
lizhongyuan <scyclzy@gmail.com>
2017-04-27 17:07:14 +0800

    start RunActivity

--- src/com/bignerdranch/android/runtracker/RunListFragment.java
+++ src/com/bignerdranch/android/runtracker/RunListFragment.java
@@ -12,6 +12,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.bignerdranch.android.runtracker.RunDatabaseHelper.RunCursor;
@@ -62,6 +63,14 @@ public class RunListFragment extends ListFragment {
 	public void onDestroy() {
 		mCursor.close();
 		super.onDestroy();
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		// The id argment will be the Run ID; CursorAdapter gives us this for free
+		Intent i = new Intent(getActivity(), RunActivity.class);
+		i.putExtra(RunActivity.EXTRA_RUN_ID, id);
+		startActivity(i);
 	}
 
 	private static class RunCursorAdapter extends CursorAdapter {
